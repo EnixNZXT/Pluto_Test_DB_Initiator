405916d29cd5a6a653f178eab3e86c9e9be670f9
EnixNZXT <91321320+EnixNZXT@users.noreply.github.com>
2023-02-14 15:14:41 +0100

    adding args

--- ptdi.py
+++ ptdi.py
@@ -2,52 +2,34 @@ import os
 import csv
 import mysql.connector
 from mysql.connector import errorcode
+import sys
+from datetime import datetime
 
+setup=sys.argv[1]
+date=sys.argv[2]
 
+    
 
-
-with open("Konfiguration.txt", "r") as config_file:
-    config = dict(line.strip().split("=") for line in config_file)
+config_file=open(r"C:\Users\roosk\Documents\repo\python\pluto_test_db_initiator\Konfiguartion.txt","r")
+con=dict(line.strip().split("=") for line in config_file)
+#print(con["host"]
+    #dict(line.strip().split("=") for line in config_file)
 # 1. Datenbankverbindung
-connection = mysql.connector.connect(user=config["user"],
-                                     password=config["password"],
-                                     host=config["host"],
-                                     database=config["database"])
-
-#cur = connection.cursor()
-
-
-
-
-
-
-
-
-
-
+#connection = mysql.connector.connect(user=config["user"],
+#                                     password=config["password"],
+#                                     host=config["host"],
+#                                     database=config["database"])
     # 3. Setup-Datei lesen
-with open(config["setup_file"], "r") as setup_file:
-        setup = dict(line.strip().split("=") for line in setup_file)
+# with open(config["setup_file"], "r") as setup_file:
+# setup = dict(line.strip().split("=") for line in setup_file)
 
     # 4. Dateipfade auf Existenz der Datei prüfen
-    csv_file_path = setup["csv_file_path"]
-    if not os.path.isfile(csv_file_path):
-        print(f"Error: CSV file not found at path {csv_file_path}")
-        return
+#csv_file_path = setup["csv_file_path"]
+#if not os.path.isfile(csv_file_path):
+ #   print(f"Error: CSV file not found at path {csv_file_path}")
 
-    # 5. CSV-Datei in Datenbank schreiben
-    table_name = setup["table_name"]
-    with open(csv_file_path, "r") as csv_file:
-        reader = csv.reader(csv_file)
-        headers = next(reader)
-        headers_str = ", ".join(headers)
-        headers_str = "(" + headers_str + ")"
-
-        query = f"INSERT INTO {table_name} {headers_str} VALUES %s"
-        cur.executemany(query, reader)
-        conn.commit()
-
-    print("Data imported successfully")
-
-if name == "main":
-    main()
+    # 5. CSV-Datei einlesen
+csv_file=open(r"C:\Users\roosk\Documents\repo\python\pluto_test_db_initiator\Setups\Teilnehmer.csv", "r")
+csv=list(line.strip().split("/") for line in csv_file)
+print(setup)
+print(date)
