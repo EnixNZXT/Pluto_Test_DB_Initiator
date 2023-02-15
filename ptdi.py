1efbff80a8be085a37dd47cd2b4099677ecd9c5c
EnixNZXT <91321320+EnixNZXT@users.noreply.github.com>
2023-02-15 16:08:38 +0100

    insert io, ausgabe count fehlt

--- ptdi.py
+++ ptdi.py
@@ -1,38 +1,48 @@
 import os
 import os.path
 import csv
-import mysql.connector
-from mysql.connector import errorcode
+#import mysql.connector
+#from mysql.connector import errorcode
 import sys
 from datetime import datetime
-
+    
+    #speichern der übergabeparameter
 setup="Setups\\"
 setup+=sys.argv[1]
 setup+=".txt"
 date=sys.argv[2]
-
+    
     #config auslesen
 config_file=open("Konfiguartion.txt","r")
+    
     #zeile für zeile einlesen, am "=" trennen, in dictonary speichern
 config=dict(line.strip().split("=") for line in config_file)
     #das referenzdatum wird aus dem dictonary gelesen, 
     #in eine varibale gepeichert, dann entfernt um den rest des Dictonarys an die connection fkt zu übergeben
 refdate=config["referenzdatum"]
-print(refdate)
 config.popitem()
-for key in config.keys():
-    print(config[key])
-    
+
     #Setup-Datei lesen
 setup_file=open(setup, "r")
 setup_dict=dict(line.strip().split(":") for line in setup_file)
+i=0
 for key in setup_dict.keys():
+    
     csv_file=open(r"csv\\"+setup_dict[key], "r")
+    setupTables=str(setup_dict[key]).strip(".csv")
+    setupTableList=list(setupTables.split(","))
     csv=list(line.strip().split(";") for line in csv_file)
-    print(csv[0])
+    #####baustelle forschleife
+    print("INSERT INTO ",setupTableList[0], csv[0], csv[i])
+   # insertTable=str(setup_dict)
+    
+    #insertValues=csv.strip("[]")
+    
+    
+    #print(csv[0])
 
     #Datenbankverbindung
-connection = mysql.connector.connect(**config) 
+#connection = mysql.connector.connect(**config) 
 
 #es gibt ein Problem mit unserer mysql version, 
 #wir kommen bis zur datenbank, werden aber am PW abgewiesen, obwohl es in workbench einwandfrei funktioniert    
