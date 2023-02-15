548913af74561f8d8fd13210c490ac266c0098f9
EnixNZXT <91321320+EnixNZXT@users.noreply.github.com>
2023-02-15 11:25:34 +0100

    codeanordnung optimiert, kommentare hinzugefügt

--- ptdi.py
+++ ptdi.py
@@ -14,12 +14,14 @@ date=sys.argv[2]
 config_file=open("Konfiguartion.txt","r")
     #zeile für zeile einlesen, am "=" trennen, in dictonary speichern
 config=dict(line.strip().split("=") for line in config_file)
+    #das referenzdatum wird aus dem dictonary gelesen, 
+    #in eine varibale gepeichert, dann entfernt um den rest des Dictonarys an die connection fkt zu übergeben
+refdate=config["referenzdatum"]
+print(refdate)
+config.popitem()
 for key in config.keys():
     print(config[key])
-
-    #Datenbankverbindung
-#connection = mysql.connector.connect(**config)
-
+    
     #Setup-Datei lesen
 setup_file=open(setup, "r")
 setup_dict=dict(line.strip().split(":") for line in setup_file)
@@ -27,3 +29,7 @@ for key in setup_dict.keys():
     csv_file=open(r"csv\\"+setup_dict[key], "r")
     csv=list(line.strip().split(";") for line in csv_file)
     print(csv[0])
+    
+    #Datenbankverbindung
+#connection = mysql.connector.connect(**config) #es gibt ein Problem mit unserer mysql version, 
+#wir kommen bis zur datenbank, werden aber am PW abgewiesen, obwohl es in workbench einwandfrei funktioniert    
